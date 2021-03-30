fa9ec78f78aaca57bab789f30976985dcd7a3385
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-03-30 08:33:50 -0400

    updated code for deployment

--- api/server.js
+++ api/server.js
@@ -6,8 +6,6 @@ const dotenv = require('dotenv');
 const path = require('path');
 const connectDB = require('./utils/db');
 
-
-
 // Define middleware
 // const customMiddleware = (req, res, next) => {
 //     console.log('middleware executed !');
@@ -19,54 +17,40 @@ const connectDB = require('./utils/db');
 // const dbName = 'pictogram';
 // const uri = `mongodb://localhost:27017/${dbName}`;
 
-console.log('__dirname', __dirname);
-console.log(process.env.MONGO_URI, process.env.NODE_ENV);
-console.log(path.resolve(__dirname, './../.env'))
+// console.log('__dirname', __dirname);
+// console.log(process.env.MONGO_URI, process.env.NODE_ENV);
+// console.log(path.resolve(__dirname, './../.env'))
 
-dotenv.config({ path: path.resolve(__dirname, './../.env')});
-console.log(process.env.MONGO_URI);
+// dotenv.config({ path: path.resolve(__dirname, './../.env')});
+dotenv.config();
+
+// connect to database
 connectDB();
 app.use(cookieParser());
 // Express body parser middleware
-app.use(express.json({ extended: false }));
-
+// app.use(express.json({ extended: false }));
+app.use(express.json());
 
 const userRoutes = require('./users/userRoutes');
 const postRoutes = require('./posts/postRoutes');
-const { connect } = require('./posts/postRoutes');
 // User Routes
 app.use('/api/users', userRoutes)
 // Post Routes
 app.use('/api/posts', postRoutes)
-
-
-
 
 // server.js at the very end of the file.
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('./build'));
     // only add this part if you are using React Router
     app.get('*', (req,res) =>{
-        console.log('__dirname', __dirname);
+        // console.log('__dirname', __dirname);
         console.log(path.join(__dirname+'/build/index.html'));
         res.sendFile(path.join(__dirname+'/build/index.html'));
     });
 }
 
-// connect to database
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(async () => {
-//         console.log(`Successfuly connected to: ${MONGO_URI}`)
-//         // Start Server
-//         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//     })
-//     .catch(err => console.log(err.message));
 
 const SERVER_PORT = process.env.SERVER_PORT || 5000;
 
-app.listen(SERVER_PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${SERVER_PORT}`));
-
-
 // Start Server
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(SERVER_PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${SERVER_PORT}`));
